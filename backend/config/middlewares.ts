85d2c02db262cc5062beb18a436469d525c26f09
Ram Miranda <569854+zordon@users.noreply.github.com>
2025-05-29 23:01:59 -0600

    s3 upload provider

--- backend/config/middlewares.ts
+++ backend/config/middlewares.ts
@@ -1,12 +1,35 @@
-export default [
-  'strapi::logger',
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+export default ({ env }) => [
+  "strapi::logger",
+  "strapi::errors",
+  {
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          "connect-src": ["'self'", "https:"],
+          "img-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            env("CDN_HOST")
+          ],
+          "media-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            env("CDN_HOST")
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+  "strapi::cors",
+  "strapi::poweredBy",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
 ];
